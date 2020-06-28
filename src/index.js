7ed4e95b4b66cb2536117855af9cdb8ae1c881c1
hahaha <fan.zhao@clobotics.com>
2020-06-28 17:17:23 +0800

    add babel

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { resolve } = require("core-js/fn/promise");
+
+let array = [1,2,3,4,5,6];
+array.includes(function(item){
+    return item>2;
+})
+console.log(111)
+class Robot {
+    constructor (msg) {
+        this.message = msg
+    }
+    say () {
+        alertMe(this.message)
+    }
+}
+Object.assign({},{
+    a:1,b:2
+})
+const fn = () => 1;
+
+
